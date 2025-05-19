8ff4f39c537532e2b12de8f5276efd54e8b3f219
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-05-19 19:31:49 +0200

    Add header section

---
+++ anton-lily/src/components/Header.jsx
@@ -0,0 +1,17 @@
+export function Header() {
+
+    const headerLinks = ['Home','About','Services','Gallery','See & Do','Contact',]
+    return (
+        <>
+            <div className="flex flex-row gap-30 justify-center bg-black w-full py-8">
+                {headerLinks.map(link=>{
+                    return (
+                        <div className="uppercase font-raleway transition-all duration-500 text-white/80 hover:text-white/50 text-sm cursor-pointer" >
+                            {link}
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    );
+}
